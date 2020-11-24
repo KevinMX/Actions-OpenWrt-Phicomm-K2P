c911d216feb693ae8e777172283115db2444d4d4
KevinMX <aaawqw@live.cn>
2020-11-24 17:13:16 +0800

    diy-part1: add some packages

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
